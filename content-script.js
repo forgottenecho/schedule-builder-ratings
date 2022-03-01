ecd32af7352a45323d82446054ed0bfbc653acbc
forgottenecho <matv201@gmail.com>
2022-02-28 19:29:39 -0500

    scraping names, load working

--- content-script.js
+++ content-script.js
@@ -1,1 +1,41 @@
-console.log("Loading up RateMyProfessor for schedule builder!")
+console.log("Loading up RateMyProfessors for schedule builder!");
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function init() {
+    const DELAY = 5000;
+
+    var pane = document.getElementsByClassName("course-list");
+    if (pane.length >= 1) {
+        // add in the ratings after DELAY ms
+        setTimeout(update, DELAY);
+
+        // attach necessary listenres to update ratings
+        // FIXME
+
+        // stop waiting for the page to load
+        document.body.removeEventListener("mousemove", init);
+    }
+}
+function update() {
+    // snag up all professors on the page
+    var profs = document.getElementsByClassName("sec-instructor");
+
+    // name formattingL [last], [first]
+    for (var i=0; i<profs.length; i++) {
+        var name = profs[i].innerText;
+        console.log(name);
+    }
+
+    return profs.length;
+}
+
+document.body.addEventListener("mousemove", init);
